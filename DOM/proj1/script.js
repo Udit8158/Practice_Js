6735c0e29f842e9742ba0264ced4c3aacdcf9b68
Udit8158 <kunduudit8158@gmail.com>
2022-05-28 19:35:30 +0530

    Fixing Bugs

--- DOM/proj1/script.js
+++ DOM/proj1/script.js
@@ -1,6 +1,6 @@
-//Grab the counter 
-const counter = document.querySelector('.counter');
-let followers = document.querySelector('.followers')
+//Grab the counter
+const counter = document.querySelector(".counter");
+let followers = document.querySelector(".followers");
 
 //Change the counter to number
 let counterNumber = Number(counter.innerHTML);
@@ -8,15 +8,15 @@ let counterNumber = Number(counter.innerHTML);
 
 //Function to change the counter number
 let counterChange = () => {
-    counterNumber ++;
-    counter.innerHTML = counterNumber;
-}
+  counterNumber++;
+  counter.innerHTML = counterNumber;
+};
 
 //Set the interval to change the counter number
-let myInterval = setInterval(counterChange,0.1);
+let myInterval = setInterval(counterChange, 0.1);
 
 //After some time it will stop the changing the counter number  (You can also do it with if statement in setInterval function)
-setTimeout(()=>{
-    clearInterval(myInterval);
-    followers.innerHTML = `Congratulation! You reach ${counterNumber} followers in YouTube.`
-},5000)
+setTimeout(() => {
+  clearInterval(myInterval);
+  followers.innerHTML = `Congratulation! You reach ${counterNumber} followers in YouTube.`;
+}, 5000);
--- DOM/proj2/script.js
+++ DOM/proj2/script.js
@@ -1,40 +1,36 @@
-console.log("Welcome");
-
-let colorElements = document.querySelectorAll('.color'); //Grab the array of color elements
-let centerSpace = document.querySelector('.center'); //Grab the center space
+let colorElements = document.querySelectorAll(".color"); //Grab the array of color elements
+let centerSpace = document.querySelector(".center"); //Grab the center space
 
 //Grab all individual color element
-let redElement = document.querySelector('.red');
-let cyanElement = document.querySelector('.cyan');
-let violetElement = document.querySelector('.violet');
-let orangeElement = document.querySelector('.orange');
-let pinkElement = document.querySelector('.pink');
+let redElement = document.querySelector(".red");
+let cyanElement = document.querySelector(".cyan");
+let violetElement = document.querySelector(".violet");
+let orangeElement = document.querySelector(".orange");
+let pinkElement = document.querySelector(".pink");
 
-
-//Function to find color 
+//Function to find color
 const findColor = (element) => {
-    return window.getComputedStyle(element).backgroundColor;
-}
+  return window.getComputedStyle(element).backgroundColor;
+};
 
 //Function to change the color in the center area
 const magicColorChange = (element) => {
-    color = findColor(element);
-    centerSpace.style.backgroundColor = color;
-}
+  color = findColor(element);
+  centerSpace.style.backgroundColor = color;
+};
 
-redElement.addEventListener('mouseenter',()=>{
-    magicColorChange(redElement);
+redElement.addEventListener("mouseenter", () => {
+  magicColorChange(redElement);
 });
-cyanElement.addEventListener('mouseenter',()=>{
-    magicColorChange(cyanElement);
+cyanElement.addEventListener("mouseenter", () => {
+  magicColorChange(cyanElement);
 });
-violetElement.addEventListener('mouseenter',()=>{
-    magicColorChange(violetElement);
+violetElement.addEventListener("mouseenter", () => {
+  magicColorChange(violetElement);
 });
-orangeElement.addEventListener('mouseenter',()=>{
-    magicColorChange(orangeElement);
+orangeElement.addEventListener("mouseenter", () => {
+  magicColorChange(orangeElement);
 });
-pinkElement.addEventListener('mouseenter',()=>{
-    magicColorChange(pinkElement);
+pinkElement.addEventListener("mouseenter", () => {
+  magicColorChange(pinkElement);
 });
-
