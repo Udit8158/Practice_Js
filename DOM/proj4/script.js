27c2fff677e620b51e06231e148c638c03943729
Udit8158 <kunduudit8158@gmail.com>
2022-05-31 10:22:56 +0530

    Fixing bugs

--- DOM/proj4/script.js
+++ DOM/proj4/script.js
@@ -1,81 +1,73 @@
-// Grab some required elements
+//// SELF CODE VIDEO ON OBS //////////
+
+// Grabing the elements
 const cards = document.querySelectorAll(".card");
 const turnCountSection = document.querySelector("#turnCountSection");
 
-// Defining variables
-let isFilipped = false;
-let turnCount = 0;
-let firstCard, secondCard;
+//Logic
+let firstCard;
+let secondCard;
+let isFillped = false;
+turnCount = 0;
 
-// Writting the methods
-// flip for flipping when click and track card and turn records
+// Making some functions
+
+// Flipping the cards
 function flip() {
-  this.classList.add("flip");
-  // Cross logic
-  if (!isFilipped) {
-    isFilipped = true;
-    // console.log(firstCard);
+  if (!isFillped) {
     firstCard = this;
-    // console.log(firstCard);
+    firstCard.classList.add("flip");
+    firstCardImg = firstCard.dataset.image;
+
+    firstCardOrder = firstCard.style.order;
+    isFillped = true;
   } else {
-    // console.log(secondCard);
     secondCard = this;
-    // console.log(secondCard);
-    turnCount += 1;
-    // console.log(firstCard, secondCard);
-    cheeckWin();
+    secondCard.classList.add("flip");
+
+    secondCardOrder = secondCard.style.order;
+    secondCardImg = secondCard.dataset.image;
+    isFillped = false;
+    checkWin();
   }
 }
 
-// cheeckWin cheeck the two cards are same or not by their data atribute
-function cheeckWin() {
-  if (firstCard.dataset.image === secondCard.dataset.image) {
+// check Win
+function checkWin() {
+  if (firstCardImg === secondCardImg && firstCardOrder !== secondCardOrder) {
     success();
   } else {
-    failure();
+    fail();
   }
+  turnCount++;
 }
 
-// success method reveal all cards and turn after being succeed
+// success
 function success() {
-  // console.log("success");
-  setTimeout(() => {
-    cards.forEach((card) => card.classList.add("flip"));
-    turnCountSection.innerHTML = `You completed this Mind Game in ${turnCount} turns`;
-  }, 500);
-  setTimeout(() => {
-    document.location.reload(); // reload page after 5s
-  }, 5000);
-  // console.log(turnCount);
+  cards.forEach((card) => {
+    card.classList.add("flip");
+  });
+  turnCount++;
+  turnCountSection.innerHTML = `Congrats, You passed the game in ${turnCount} turn`;
 }
 
-// failure method flip back two cards and rest
-function failure() {
-  // console.log("failure");
+// Failure
+function fail() {
   setTimeout(() => {
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
-    reset();
   }, 500);
 }
 
-// reset method reset the value of variables back to their first state except turnCount
-function reset() {
-  isFilipped = false;
-  [firstCard, secondCard] = [null, null]; // Use desturcture
+// shuffle
+function shuffle() {
+  cards.forEach((card) => {
+    randomOrder = Math.floor(Math.random() * 16);
+    card.style.order = randomOrder;
+  });
 }
 
-// TODO: shuffle
-
-// shuffle method suffeling the all cards and run selfly
-(function shuffle() {
-  cards.forEach((card) => {
-    let index = Math.floor(Math.random() * 16);
-    card.style.order = index;
-    cards.forEach((card) => card.classList.remove("flip"));
-  });
-})();
-// window.onload = shuffle();
-
-// Logic apply for the game
-cards.forEach((card) => card.addEventListener("click", flip));
+shuffle();
+cards.forEach((card) => {
+  card.addEventListener("click", flip);
+});
