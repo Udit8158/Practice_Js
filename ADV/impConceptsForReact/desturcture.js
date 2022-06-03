3d107bfaf0d62402db52c9ec06ae5f541b77160c
Udit8158 <kunduudit8158@gmail.com>
2022-06-03 23:56:16 +0530

    More on destructure

--- ADV/impConceptsForReact/desturcture.js
+++ ADV/impConceptsForReact/desturcture.js
@@ -9,13 +9,13 @@ let myObj = {
   myName: "Udit",
   age: "18",
 };
-let { myName: name, age } = myObj;
+// let { myName: name, age } = myObj;
 console.log(name); // Udit  (Means if you want to change the name of the variable then  like this....)
 
 // Destructuring of nested array...
 let arr = ["Udit", 12, 2004, ["JS", "Python"]];
 
-let [Name, Age, birthYear, programmingLanguage] = arr;
+// let [Name, Age, birthYear, programmingLanguage] = arr;
 let [second, first] = programmingLanguage;
 
 console.log(Name, programmingLanguage, second);
@@ -37,3 +37,49 @@ let {
 } = employee; // If you want to change a name.
 
 console.log(role, jobSalary, f);
+
+///// More on destructure ///////////
+
+// Array Destructuring
+const arr1 = [1, 2, 3, 4];
+const arr2 = ["a", "b", "c", "d"];
+
+const [A, B, ...rest] = arr1;
+const [C, , E] = arr2; // Means C = a and E = c (skip 'b')
+console.log(A, E, rest); // Rest - [3,4] using rest operator
+
+const arr3 = [...arr1, ...arr2];
+console.log(arr3); // Concadination of multiple array
+
+// Object destructuring
+const obj1 = {
+  name: "Udit",
+  age: 18,
+  woks: {
+    code: "Vs code",
+    company: "Microsoft",
+  },
+};
+
+const obj2 = {
+  role: "Web Dev",
+  programmingLanguage: "JavaScript",
+};
+
+const { name: Name, age, favFood = "Briyani" } = obj1; // Setting default value of favFood
+console.log(Name, favFood); // Udit (changing 'name' into "Name")
+
+const obj3 = { ...obj1, ...obj2 };
+console.log(obj3); // Conadination object (IMP)
+
+const objFunc = (obj) => {
+  console.log(`Name is ${obj.name} and age is ${obj.age}`);
+};
+objFunc(obj1);
+
+// Using destructure inside function (Very important for react)
+const objAdvFunc = ({ name, age }) => {
+  console.log(`Name is ${name} and age is ${age}`);
+};
+
+objAdvFunc(obj1);
--- practice.js
+++ practice.js
@@ -36,4 +36,26 @@ const print = async () => {
   await enrollStudent(newStudent);
   fetchStudents();
 };
-print();
+// print();
+
+// Spread and rest operator
+//TODO: Spread : it use when you want to pass array or other params as arg on the time of calling a func.
+
+const sumTwoNumber = (a, b) => a + b;
+
+const sumResult = sumTwoNumber(...[3, 4]);
+// console.log(sumResult);
+
+// Rest : it allows a function to take infinite number of args when creation (Actually it creates an array of multiple args.So we ne)
+
+const multTwoSumOthers = (a, b, ...args) => {
+  let sum = 0;
+  let mult = a * b;
+  args.forEach((arg) => {
+    sum += arg;
+  });
+  return [mult, sum];
+};
+
+const multFuncResult = multTwoSumOthers(2, 3, 3, 3);
+// console.log(multFuncResult);
