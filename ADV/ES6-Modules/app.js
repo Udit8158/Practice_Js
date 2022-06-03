4a2a67fe9f47f496e10cdcc90b77cb0377841001
Udit8158 <kunduudit8158@gmail.com>
2022-06-03 21:47:32 +0530

    import export practice.

--- ADV/ES6-Modules/app.js
+++ ADV/ES6-Modules/app.js
@@ -1,7 +1,5 @@
-import { User, default as d } from "./modules/user.js";
-import * as acc from "./modules/account.js";
+// For default's export use outside {} and for normal exprot-import use inside {}
+import User, { userName, userAge } from "./modules/user.js";
 
-const usr1 = new User("Udit", 18, "20 LPA", "Developer");
-console.log(usr1.deposit(12490));
-console.log(d());
-console.log(acc.sayHi());
+const usr1 = new User("Udit", 18);
+console.log(usr1);
