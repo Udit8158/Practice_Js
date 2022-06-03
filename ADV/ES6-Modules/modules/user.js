e2f132b419e87edeea64af146b71baf377a266d7
Udit8158 <kunduudit8158@gmail.com>
2022-06-03 21:47:53 +0530

    module

--- ADV/ES6-Modules/modules/user.js
+++ ADV/ES6-Modules/modules/user.js
@@ -1,19 +1,15 @@
-// Make a class
-class User {
-  constructor(name, age, salary, role) {
-    (this.name = name),
-      (this.age = age),
-      (this.salary = salary),
-      (this.role = role);
-  }
-  deposit(depositAmount) {
-    return `${this.name} deposit ${depositAmount} money.`;
+// Default export only can one.
+
+export default class User {
+  constructor(name, age) {
+    (this.name = name), (this.age = age);
   }
 }
-// Exporting default function
-export default () => {
-  return "This is default export.";
-};
-const name = "Hitesh";
 
-export { User };
+export const userName = (usr) => usr.name;
+export const userAge = (usr) => usr.age;
+
+// Also can do this...
+
+// export default User;
+// export { userName, userAge };
